4d12bd3162ec9aa3a99dc0365c73c5256e5638e1
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-05-03 04:04:12 -0700

    01: Introduction

---
+++ 01-Introduction/main.c
@@ -0,0 +1,60 @@
+// Introcuction to Operating Systems
+
+#include <stdio.h>
+
+int main(int argc, char const* argv[]) {
+
+    // Operating Systems.
+    // Operating System is a program, that manages, the computer hardware.
+    // It also provides a basis for application programs, and acts as an
+    // intermediary between computer user and computer hardware.
+
+    // What is computer hardware?
+    // Computer hardware is basically electronic devices liek RAM, CPU,
+    // I/O devices, etc... attached to the motherboard.
+
+    // Software
+    // There are 2 types of software.
+    // 1. System Software
+    // 2. Application Software
+
+    // System Software
+    // System softwares are the software that are used to command directly the system.
+    // Basically directly hardware.
+    // Operating system is also a system software.
+    // Drivers are also system softwares.
+    
+    // Application Software
+    // Application softwares are used to perform a specific task.
+    // Like, text editor is a application program used to write text.
+    // Compilers are also application softwares, like they are used to
+    // compile language code.
+    
+    // Basically the Operating System is kind of a translator,
+    // Which will translate from both sides.
+    // Like when we have to write code, we open a text editor,
+    // using Operating System, and when we save the file,
+    // The operating system tells computer hardware that write this thing,
+    // into hard drive. Etc...
+
+    // Types of Operating System:
+    // Batch OS
+    // Time sharing OS
+    // Distributed OS
+    // Network OS
+    // Real time OS
+    // Multi Programming / Tasking / Processing OS
+    // etc...
+    
+    // These are the main types of Operating Systems.
+
+    // Some Functions of OS:
+    // It is an interface between User and Hardware.
+    // Allocation of Resources.
+    // Management of Memory, Security, etc...
+
+    // Operating System gives us Convenience of using Computer.
+    
+    
+    return 0;    
+}
